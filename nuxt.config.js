02da87b849b673619df0f3dc339f6a23dbc8d85c
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-07 11:59:42 +0100

    nuxt config - changed peerjs cdn

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
-      { src: 'https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js' }
+      { src: 'https://unpkg.com/peerjs@1.3.2/dist/peerjs.min.js' }
     ]
   },
 
