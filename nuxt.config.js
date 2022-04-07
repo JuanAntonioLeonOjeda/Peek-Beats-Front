9c56f1b8657f10be8b7f9d65737620886284b5a6
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-07 19:53:34 +0100

    nuxt config ssr false

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
 
   // target: 'static',
-  // ssr: false,
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - peek-beats-front',
